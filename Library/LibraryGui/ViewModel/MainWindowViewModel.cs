4e373144571a96845bb29155f14d7182171d9a6c
Dezodemius <gladkovyegor@gmail.com>
2020-07-11 23:47:49 +0400

    Добавил GUI WPF

---
+++ Library/LibraryGui/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Library;
+using LibraryGui.Utils;
+
+namespace LibraryGui.ViewModel
+{
+  public class MainWindowsViewModel : ViewModel
+  {
+    public override string Name { get; } = "Главное окно";
+    
+    private DelegateCommand _searchCommand;
+    
+    public DelegateCommand SearchCommand => _searchCommand ?? (_searchCommand = new DelegateCommand(Search, SearchCanExecute));
+
+    private bool SearchCanExecute(object arg)
+    {
+      return true;
+    }
+
+    private void Search(object obj)
+    {
+      var esProvider = new ElasticProvider();
+      var searchPhrase = Console.ReadLine();
+      var documents = esProvider.Search(searchPhrase).Documents;
+      foreach (var result in documents)
+        Console.WriteLine(result.Name);
+    }
+  }
+}
