577f94e37a0bd9794d042c2d6c21ec2430f0f412
Dezodemius <gladkovyegor@gmail.com>
2020-07-18 20:49:13 +0400

    Изменения в файле проекта
    - Переименовал LibraryGui в LibraryClient.
    - Добавил фавикон.
    - Добавил информацию о проекте.

--- Library/LibraryGui/Properties/AssemblyInfo.cs
+++ Library/LibraryGui/Properties/AssemblyInfo.cs
@@ -8,10 +8,10 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("LibraryGui")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Library on Elasticsearch")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LibraryGui")]
+[assembly: AssemblyCompany("Gladkov Yehor")]
+[assembly: AssemblyProduct("Library")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -53,3 +53,4 @@ using System.Windows;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
