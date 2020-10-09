317dd66fb0f17d17b776da6f5ee2d71ef2c7ff5f
Dezodemius <gladkovyegor@gmail.com>
2020-10-09 04:20:01 +0400

    Настроил поиск с помощью русской морфологии
    - Добавил создание индекса и поиск с учётом русской морфологии.

--- Library/Library/Utils/ElasticProvider.cs
+++ Library/Library/Utils/ElasticProvider.cs
@@ -168,24 +168,25 @@ namespace Library.Utils
     /// <returns>Результат поиска.</returns>
     public ISearchResponse<Book> Search(string searchPhrase)
     {
-      ISearchResponse<Book> response = new SearchResponse<Book>();
-      // try
-      // {
-      //   _log.Debug($"Searching: {searchPhrase} in index: {DefaultIndex}");
-      //   response = Client.Search<Book>(s => s
-      //     .Query(q => q
-      //       .MultiMatch(c => c
-      //         .Query(searchPhrase)
-      //         .Fields(
-      //           f => f.Field(b => b.Text))))
-      //     .Highlight(h => h
-      //       .Fields(f => f.Field(b => b.Text))));
-      // }
-      // catch (Exception e)
-      // {
-      //   _log.Error(e, e.StackTrace);
-      //   throw;
-      // }
+      ISearchResponse<Book> response;
+      try
+      {
+        _log.Debug($"Searching: {searchPhrase} in index: {DefaultIndex}");
+        response = Client.Search<Book>(s => s
+          .Query(q => q
+            .MultiMatch(c => c
+              .Query(searchPhrase)
+              .Analyzer("russian_morphology")
+              .Fields(
+                f => f.Field(b => b.Attachment.Content))))
+          .Highlight(h => h
+            .Fields(f => f.Field(b => b.Attachment.Content))));
+      }
+      catch (Exception e)
+      {
+        _log.Error(e, e.StackTrace);
+        throw;
+      }
 
       return response;
     }
@@ -264,7 +265,21 @@ namespace Library.Utils
         Client.Indices.Create(DefaultIndex, 
           i => i
             .Map<Book>(m => m
-              .AutoMap()));
+              .AutoMap()
+              .Properties(p => p
+                .Text(t => t
+                  .Name(n => n.Attachment.Content)
+                  .SearchAnalyzer("russian_morphology"))))
+            .Settings(s => s
+              .Analysis(a => a
+                .TokenFilters(f => f
+                  .Stemmer("russian_stemmer", s => s.Language("russian"))
+                  .Stop("morphology_stopwords", w => w.StopWords("а,без,более,бы,был,была,были,было,быть,в,вам,вас,весь,во,вот,все,всего,всех,вы,где,да,даже,для,до,его,ее,если,есть,еще,же,за,здесь,и,из,или,им,их,к,как,ко,когда,кто,ли,либо,мне,может,мы,на,надо,наш,не,него,нее,нет,ни,них,но,ну,о,об,однако,он,она,они,оно,от,очень,по,под,при,с,со,так,также,такой,там,те,тем,то,того,тоже,той,только,том,ты,у,уже,хотя,чего,чей,чем,что,чтобы,чье,чья,эта,эти,это,я,a,an,and,are,as,at,be,but,by,for,if,in,into,is,it,no,not,of,on,or,such,that,the,their,then,there,these,they,this,to,was,will,with")))
+                .Analyzers(aa => aa
+                  .Custom("russian_morphology", c => c
+                    .Tokenizer("standard")
+                    .Filters("lowercase", "russian_morphology", "english_morphology", "morphology_stopwords", "russian_stemmer"))))));
+       
         _log.Info($"Index {DefaultIndex} created.");
       }
       else
