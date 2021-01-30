6358ab773c21d12c8753c6f740f050cb769d4ac6
Егор <gladkovyegor@gmail.com>
2021-01-31 00:57:35 +0400

    Создал App.config, добавил класс для работы с настройками конфигурациий
    - Создал файл App.config, содержащий некоторые настройки для приложения.
    - Создал класс ApplicationSettings, которые обрабатывает эти настройки.

---
+++ Library/Library.Client/Settings/ApplicationSettings.cs
@@ -0,0 +1,36 @@
+﻿using System.Configuration;
+using System.IO;
+
+namespace Library.Client.Settings
+{
+  /// <summary>
+  /// Настройки приложения.
+  /// </summary>
+  public static class ApplicationSettings
+  {
+    /// <summary>
+    /// Признак необходимости запустить ES при старте.
+    /// </summary>
+    internal static bool NeedRunElasticOnStartup => bool.Parse(GetSetting("START_ELASTIC_ON_STARTUP"));
+
+    /// <summary>
+    /// Признак необходимости закрыть ES после выхода из приложения.
+    /// </summary>
+    internal static bool NeedCloseElasticOnExit => bool.Parse(GetSetting("CLOSE_ELASTIC_ON_EXIT"));
+
+    /// <summary>
+    /// Путь, где находится сервис ES.
+    /// </summary>
+    internal static FileInfo ElasticsearchAppFileInfo => new FileInfo(GetSetting("ELASTICSEARCH_SERVICE_PATH"));
+
+    /// <summary>
+    /// Получить настройку из App.config.
+    /// </summary>
+    /// <param name="settingName">Имя настройки.</param>
+    /// <returns>Значение настройки. Имеет тип <c>string</c></returns>
+    private static string GetSetting(string settingName)
+    {
+      return ConfigurationManager.AppSettings.Get(settingName);
+    }
+  }
+}
