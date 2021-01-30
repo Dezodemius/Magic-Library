3f31438bd1d8e568e003c299d536596ef915fa91
Егор <gladkovyegor@gmail.com>
2021-01-31 01:08:17 +0400

    Значения по умолчанию для настроек из App.config

--- Library/Library.Client/Settings/ApplicationSettings.cs
+++ Library/Library.Client/Settings/ApplicationSettings.cs
@@ -1,4 +1,5 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Configuration;
 using System.IO;
 
 namespace Library.Client.Settings
@@ -11,17 +12,43 @@ namespace Library.Client.Settings
     /// <summary>
     /// Признак необходимости запустить ES при старте.
     /// </summary>
-    internal static bool NeedRunElasticOnStartup => bool.Parse(GetSetting("START_ELASTIC_ON_STARTUP"));
+    internal static bool NeedRunElasticOnStartup
+    {
+      get
+      {
+        var defaultValue = bool.TrueString;
+        var settingValue = GetSetting("START_ELASTIC_ON_STARTUP");
+        return bool.Parse(settingValue ?? defaultValue);
+      }
+    }
 
     /// <summary>
     /// Признак необходимости закрыть ES после выхода из приложения.
     /// </summary>
-    internal static bool NeedCloseElasticOnExit => bool.Parse(GetSetting("CLOSE_ELASTIC_ON_EXIT"));
+    internal static bool NeedCloseElasticOnExit
+    {
+      get
+      {
+        var defaultValue = bool.TrueString;
+        var settingValue = GetSetting("CLOSE_ELASTIC_ON_EXIT");
+        return bool.Parse(settingValue ?? defaultValue);
+      }
+    }
 
     /// <summary>
     /// Путь, где находится сервис ES.
     /// </summary>
-    internal static FileInfo ElasticsearchAppFileInfo => new FileInfo(GetSetting("ELASTICSEARCH_SERVICE_PATH"));
+    internal static FileInfo ElasticsearchAppFileInfo
+    {
+      get
+      {
+        var settingValue = GetSetting("ELASTICSEARCH_SERVICE_PATH");
+        if (settingValue == null)
+          throw new ArgumentException("Не задан путь к сервису Elasticsearch. " +
+                                      "Для указания пути используйте настойку ELASTICSEARCH_SERVICE_PATH");
+        return new FileInfo(settingValue);
+      }
+    }
 
     /// <summary>
     /// Получить настройку из App.config.
