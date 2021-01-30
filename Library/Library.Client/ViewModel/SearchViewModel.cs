af067666c9c11d8efacc328e69fda87a90b58cf6
Егор <gladkovyegor@gmail.com>
2021-01-31 02:37:22 +0400

    App.manifest + SplashScreen
    - Добавил app.manifest.
    - Добавил splashscreen при старте приложения.

--- Library/Library.Client/ViewModel/SearchViewModel.cs
+++ Library/Library.Client/ViewModel/SearchViewModel.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
-using System.Linq;
 using Library.Client.Utils;
 using Library.Entity;
 using Library.Utils;
